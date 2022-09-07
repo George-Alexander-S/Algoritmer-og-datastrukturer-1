7261b55f3018cded15cc543426f5241186a88d79
Gromfader <88212297+Gromfader@users.noreply.github.com>
2022-09-07 14:34:59 +0200

    Oppgave 1 - Implementerte kode som til slutt kom seg gjennom alle testene. Brukte alt for lang tid på å skjønne at det ikke skulle stå UnsupportedOperationException i koden. Deretter tok det aaalt for lang tid før jeg leste resten av oppgaveteksten, slik at jeg hadde ikke gjort noe i ombyttinger metoden. Etter det gikk det smidig med oppgave 1.

--- src/main/java/no/oslomet/cs/algdat/Oblig1/Oblig1.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig1/Oblig1.java
@@ -3,17 +3,48 @@ package no.oslomet.cs.algdat.Oblig1;
 ////// Løsningsforslag Oblig 1 ////////////////////////
 
 import java.lang.UnsupportedOperationException;
+import java.util.NoSuchElementException;
 
 public class Oblig1 {
     private Oblig1() {}
 
     ///// Oppgave 1 //////////////////////////////////////
     public static int maks(int[] a) {
-        throw new UnsupportedOperationException();
+        if (a.length == 0) { // Sjekker at lengden på tabellen er større enn 0
+            throw new NoSuchElementException("Tabellen er tom og har dermed ingen verdier");
+            // Brukte ALT for lang tid på å skjønne at jeg skulle bruke en annen feilmelding enn den som stod her fra før
+        }
+        else { // Dersom tabellen ikke er tom, så kjører vi.
+            int max = a[0];        // Initialiserer max som 0.
+
+            for (int i = 1; i < a.length; i++) { // En forløkke som starter med indeks 1 og sammenligner med forrige før inkrementering
+                if (a[i-1] > a[i]) {
+                    int temp = a[i];
+                    a[i] = a[i-1];
+                    a[i-1] = temp;
+                }
+                max = a[i];
+            }
+            return max;
+        }
     }
 
     public static int ombyttinger(int[] a) {
-        throw new UnsupportedOperationException();
+        if (a.length == 0) { // Sjekker at lengden på tabellen er større enn 0
+            throw new NoSuchElementException("Tabellen er tom og har dermed ingen verdier");
+        } else {
+            int bytter = 0;        // Initialiserer antall bytter som 0.
+
+            for (int i = 1; i < a.length; i++) {
+                if (a[i-1] > a[i]) {
+                    int temp = a[i];
+                    a[i] = a[i-1];
+                    a[i-1] = temp;
+                    bytter ++;
+                }
+            }
+            return bytter;
+        }
     }
 
     ///// Oppgave 2 //////////////////////////////////////
